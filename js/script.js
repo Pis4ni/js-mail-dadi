c845d0af91f07f1ac4ab750f0c304e467cb136b8
Pis4ni <pis4wrk@gmail.com>
2023-07-24 16:01:32 +0200

    add: on click logic mail

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-//! -------------- MAIL ---------------!//
+/* //! -------------- MAIL ---------------!//
+
 // immagazzino la lista di mail accettate
 let acceptedMail =['a@gmail.com', 'b@gmail.com','c@gmail.com']
 //  chiedo l' e-mail dell' utente
-const userEmail = prompt('inserisci la tua E-mail')
+//todo const userEmail = prompt('inserisci la tua E-mail')
 // creo una variabile per determinare la possibilità di accedere
 let denied = true
 // creo un ciclo per controllare la lista delle e-mail accettate
@@ -22,7 +23,7 @@ if (denied == true) {
 }else if(denied == false){
     // stampo a schermo un messaggio di accesso consentito
     console.log('La tua E-mail è tra quelle con accesso consentito')  
-}
+} */
 //! -------------- DADI ---------------!//
 // creo una variabile random per il valore del dado del player
 let randomPlayer = Math.floor(Math.random() * 6) + 1
@@ -45,3 +46,34 @@ if (randomPc>randomPlayer) {
     // mostro a schermo il messaggio di vittoria
     console.log('Hai Vinto!')
 }
+
+
+let logInBtn = document.getElementById('log_in_btn')
+// immagazzino la lista di mail accettate
+let acceptedMail =['a@gmail.com', 'b@gmail.com','c@gmail.com']
+
+logInBtn.addEventListener('click', function(){
+    //  chiedo l' e-mail dell' utente
+    let userEmail = document.getElementById('e_mail').value
+    //todo const userEmail = prompt('inserisci la tua E-mail')
+    // creo una variabile per determinare la possibilità di accedere
+    let denied = true
+    // creo un ciclo per controllare la lista delle e-mail accettate
+    for (let i = 0; i < acceptedMail.length; i++) {
+        // SE è presente tra quelle con accesso consentito concedo l' accesso
+        if (userEmail== acceptedMail[i]) {
+            // modifico il valore della variabile che determina la possibilità di accedere
+            denied = false
+        }
+    }
+    // stampo a schermo il risultato
+    //SE il valore della variabile che determina la possibilità di accedere è true
+    if (denied == true) {
+        //stampo a schermo un messaggio di accesso negato
+        console.log('Spiacente, non hai accesso a questa sezione')
+        //ALTRIMENTI
+    }else if(denied == false){
+        // stampo a schermo un messaggio di accesso consentito
+        console.log('La tua E-mail è tra quelle con accesso consentito')  
+    }
+})
